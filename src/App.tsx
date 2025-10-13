fade018212cf9fca98977c0d52db5858daa55157
Ethan <chinshi@foxmail.com>
2025-10-13 20:17:58 +0800

    Add initial setup with Mastra client integration and UI components
    - Created .env file for API URL configuration.
    - Updated eslint configuration to ignore specific directories.
    - Added dependencies for Mastra client and AI SDK.
    - Implemented a loading spinner component.
    - Enhanced App component to interact with the Mastra API and display results.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { mastraClient } from "@/lib/mastra-client";
+import { useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [msg, setMsg] = useState<string>("");
+  const handleClick = async () => {
+    setLoading(true);
+    const agent = mastraClient.getAgent("weatherAgent");
+    const stream = await agent.stream({
+      messages: [{ role: "user", content: "深圳天气" }],
+    });
+    stream.processDataStream({
+      onChunk: async (chunk) => {
+        console.log(chunk);
+        if (chunk.type === "step-finish") {
+          setMsg(chunk.payload.output.text ?? "");
+        }
+      },
+    });
+    setLoading(false);
+  };
+
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm md:max-w-4xl">
+          <Button disabled={loading} onClick={handleClick}>
+            {loading && <Spinner />}
+            Test Agent
+          </Button>
+          <pre>{msg}</pre>
+        </div>
       </div>
     </>
   );
